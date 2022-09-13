e65c2710f07c85696b665d2cfc6e5545b2e4c826
jhougomont <joshuabenjaminhill@gmail.com>
2022-09-13 08:46:37 -0400

    Removing unnecessary dependencies

--- annotation_explorer.py
+++ annotation_explorer.py
@@ -1,12 +1,7 @@
 import streamlit as st
 import requests
 import pandas as pd
-import numpy as np
-import geopandas as gpd
 import json
-from shapely.geometry import shape
-import ast
-import matplotlib.pyplot as plt
 
 st.title('Annotation Explorer')
 
