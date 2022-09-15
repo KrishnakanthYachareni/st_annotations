23af53ed5a9cc5ff700bcdf354cf927dfc5b421a
jhougomont <joshuabenjaminhill@gmail.com>
2022-09-14 22:35:41 -0400

    fixed video issue.

--- pages/2_Video.py
+++ pages/2_Video.py
@@ -2,6 +2,10 @@ import streamlit as st
 from annotation_explorer import get_data
 import pandas as pd
 
+#####
+# REMEMBER TO CHANGE "Annotation_Explorer" to "annotation_explorer" FOR DEPLOYMENT #
+#####
+
 # Set page title
 st.title("Video")
 
@@ -18,15 +22,17 @@ df = get_data()
 annotation_data = df[[isinstance(x, dict) for x in df['properties.metadata.tags']]]
 
 # Add keys for mp4 videos (just for testing)
-annotation_data['properties.metadata.S3Key'].iloc[3] = "EX1708/EX1708_VID_20170921T220500Z_ROVHD_Low.mp4" # for testing
-annotation_data['properties.metadata.S3Key'].iloc[11] = "EX1708/EX1708_VID_20170921T220500Z_ROVHD_Low.mp4" # for testing
+# annotation_data['properties.metadata.S3Key'].iloc[3] = "EX1708/EX1708_VID_20170921T220500Z_ROVHD_Low.mp4" # for testing
+# annotation_data['properties.metadata.S3Key'].iloc[11] = "EX1708/EX1708_VID_20170921T220500Z_ROVHD_Low.mp4" # for testing
 
 # Limit to mp4 files
 videos = annotation_data[annotation_data['properties.metadata.S3Key'].str.contains(".mp4")]
 # videos = pd.concat([mp4, annotation_data[annotation_data['properties.metadata.S3Key'].str.contains(".mp4")]])
 
+# Create values from keys (wonky dictionary column structure, so had to normalize to get keys)
 keys = pd.json_normalize(videos['properties.metadata.tags']).columns
 
+# Create dropdown
 tag_choice = st.sidebar.selectbox("Tags", options= keys)
 
 # Create list of Cloudfront URLs
@@ -35,7 +41,7 @@ for i in videos['properties.metadata.S3Key']:
     video_list.append("https://d9we9npuc9dc.cloudfront.net/{}".format(i))
 
 # Create video-selector dropdown
-video_choice = st.sidebar.number_input("Which Video?", max_value=len(video_list))
+video_choice = st.sidebar.number_input("Which Video?", min_value=0, max_value=len(video_list))
 
 # Plot Videos
 try:
